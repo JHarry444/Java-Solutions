c8c37e632dfc049707f1fa35d0894a5c786e2d18
jharry444 <jordan.harrison@qa.com>
2021-02-18 14:22:48 +0000

    explained encapsulation

--- src/oop/Runner.java
+++ src/oop/Runner.java
@@ -7,12 +7,11 @@ public class Runner {
 		p1 = new Person("JH", 26, "Java Trainer");
 		Person p2 = new Person("Nick", 26, "Weapon");
 
-		p1.print();
-		p2.print();
-
 		PersonManager manager = new PersonManager();
 		manager.addPerson(p1);
 		manager.addPerson(p2);
+
+//		manager.people.add(p1); bad encapsulation
 
 		Person found = manager.findPersonByName("JH");
 		found.print();
